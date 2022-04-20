0fecec306fc002ac6ec572b19012cfe7000f5008
simonatingleffkardel <simonakardel@gmail.com>
2022-04-20 13:24:11 +0200

    initial set up

---
+++ src/main/java/com/balls/bouncingballs/Controller.java
@@ -0,0 +1,11 @@
+package com.balls.bouncingballs;
+
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+
+public class Controller {
+
+    @FXML
+    protected AnchorPane scene;
+}
